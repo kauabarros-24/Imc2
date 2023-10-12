05fb01cbead0d4acd324435e252f68fc05a433ce
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-12 12:32:09 -0300

    Terminado

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+
 function calcularImc() {
     let altura = document.getElementById("estatura").value;
     let peso = document.getElementById("massa").value;
 
     if (altura == "") alert("Insira o valor da altura: ");
+
     else if (peso == "") alert("Insira o valor da massa corporal");
+
     else {
    
         let soma = Number(peso) / (Number(altura) * Number(altura));
+
         document.getElementById("imc").value = soma.toFixed(1);
         
         let classificacao = "";
